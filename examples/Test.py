5bd7596cb2abc4086ff912191935b8f3e63bd36f
Glenn Downing <downing@cs.utexas.edu>
2012-08-28 11:26:48 -0500

    another commit

--- examples/Test.py
+++ examples/Test.py
@@ -6,22 +6,6 @@
 
 print "Test.py"
 
-x = 5
-e = 0.01
-print "e =", e
-l = 0
-h = max(x, 1.0)
-a = (h + l) / 2.0
-print "l =", l, "a =", a, "h =", h
-print "d =", a**2 - x
-while (abs(a**2 - x) >= e) :
-    if a**2 < x :
-        l = a
-    else :
-        h = a
-    a = (h + l) / 2.0
-    print "l =", l, "a =", a, "h =", h
-    print "d =", a**2 - x
-print "a =", a
+print dir(range)
 
 print "Done."
