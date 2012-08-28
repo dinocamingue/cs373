d386e6de90b712199bdd0dc50eb3ad0ab4e4f6db
Glenn Downing <downing@cs.utexas.edu>
2012-08-28 08:33:03 -0500

    another commit

--- examples/Hello.py
+++ examples/Hello.py
@@ -8,6 +8,9 @@
 % python -V
 Python 2.6.5
 
+% python3 -V
+Python 3.1.2
+
 
 
 % python Hello.py
@@ -23,8 +26,10 @@ print "Nothing to be done."
 Python 2.6.5 (r265:79063, Apr 16 2010, 13:57:41)
 [GCC 4.4.3] on linux2
 Type "help", "copyright", "credits" or "license" for more information.
+
 >>> import Hello
 Nothing to be done.
+
 >>> quit()
 """
 
@@ -33,6 +38,45 @@ Nothing to be done.
 Python 2.6.5 (r265:79063, Apr 16 2010, 13:57:41)
 [GCC 4.4.3] on linux2
 Type "help", "copyright", "credits" or "license" for more information.
+
+>>> help()
+Welcome to Python 2.6!  This is the online help utility.
+
+If this is your first time using Python, you should definitely check out
+the tutorial on the Internet at http://docs.python.org/tutorial/.
+
+Enter the name of any module, keyword, or topic to get help on writing
+Python programs and using Python modules.  To quit this help utility and
+return to the interpreter, just type "quit".
+
+To get a list of available modules, keywords, or topics, type "modules",
+"keywords", or "topics".  Each module also comes with a one-line summary
+of what it does; to list the modules whose summaries contain a given word
+such as "spam", type "modules spam".
+
+help> range
+Help on built-in function range in module __builtin__:
+
+range(...)
+    range([start,] stop[, step]) -> list of integers
+
+    Return a list containing an arithmetic progression of integers.
+    range(i, j) returns [i, i+1, i+2, ..., j-1]; start (!) defaults to 0.
+    When step is given, it specifies the increment (or decrement).
+    For example, range(4) returns [0, 1, 2, 3].  The end point is omitted!
+    These are exactly the valid indices for a list of 4 elements.
+
+help> quit
+
+>>> quit()
+"""
+
+"""
+% python
+Python 2.6.5 (r265:79063, Apr 16 2010, 13:57:41)
+[GCC 4.4.3] on linux2
+Type "help", "copyright", "credits" or "license" for more information.
+
 >>> import this
 The Zen of Python, by Tim Peters
 
@@ -55,5 +99,6 @@ Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
+
 >>> quit()
 """
