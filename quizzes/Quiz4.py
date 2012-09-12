24fff747a3a0f1d3280a641b81a6d428c198e6ad
Glenn Downing <downing@cs.utexas.edu>
2012-09-12 16:19:47 -0500

    another commit

---
+++ quizzes/Quiz4.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+CS373: Quiz #4 (5 pts)
+"""
+
+""" ----------------------------------------------------------------------
+1. What is a user story?
+   [XP: Ch. 4, Pg. 24]
+   (1 pt)
+
+a short description of the behavior of the system, from the point of view
+of the user
+"""
+
+""" ----------------------------------------------------------------------
+2. What is a spike solution?
+   [XP: Ch. 6, Pg. 40]
+   (1 pt)
+
+an experiment, where you do enough work to get a good feeling for how to
+implement the story
+"""
+
+""" ----------------------------------------------------------------------
+3. In Python are assertions enabled or disabled by default?
+   How do you enable them or disable them?
+   (2 pts)
+
+enabled
+compile with -O
+"""
